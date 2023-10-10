e4e7b91a796b6d187c17100ae1c8c080bf4f8bf6
Caio-Henrique3 <caioaranha123@hotmail.com>
2023-10-10 09:49:48 -0300

    continuação da estilização do sidebar

--- src/main/java/com/projeto/controller/GestaoEmpresasController.java
+++ src/main/java/com/projeto/controller/GestaoEmpresasController.java
@@ -7,7 +7,6 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import com.projeto.enums.TipoEmpresa;
 import com.projeto.model.Empresa;
 import com.projeto.repository.EmpresaRepository;
 
